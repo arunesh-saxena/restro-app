e7f8f816abde5f8909f51416c2ff8d875ce89e8c
Arunesh Saxena <asaxena24@sapient.com>
2019-04-03 15:06:09 +0530

    redux-logger package added

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import logger,{createLogger} from 'redux-logger'
 import rootReducer from '../reducers/index';
 
 
