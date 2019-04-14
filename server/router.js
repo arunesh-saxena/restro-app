8d5f386b26b3c598d96285f4ec96b9f8c7089c37
Arunesh Saxena <asaxena24@sapient.com>
2019-04-14 21:29:22 +0530

    add multer for multipart form post api

--- server/router.js
+++ server/router.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var routes = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        let info = file.originalname.split('.');
+        const ext = info[info.length - 1];
+        cb(null, `${info[0]}-${Date.now()}.${ext}`)
+    }
+});
+
+var upload = multer({ storage: storage }).single('imageURL');
+
 import expressConstants from '../app/appConstants/expressEndPoint';
 import SignUpController from './controllers/SignUpController';
 import LoginController from './controllers/LoginController';
 import LogOutController from './controllers/LogOutController';
 import MyAccountController from './controllers/MyAccountController';
+import MenuController from './controllers/MenuController';
 
 /* testing */
 routes.get(expressConstants.TEST_API.url, function (req, res) {
@@ -18,6 +33,7 @@ routes.post(expressConstants.SIGN_UP.url, SignUpController);
 routes.post(expressConstants.LOGIN.url, LoginController);
 routes.post(expressConstants.LOGOUT.url, LogOutController);
 routes.post(expressConstants.ISLOGIN.url, MyAccountController);
+routes.post(expressConstants.UPLOAD_MENU.url, upload, MenuController);
 
 
 
