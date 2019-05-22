dde738f2c4d0bd898194a23c543f737a09f59172
Arunesh Saxena <asaxena24@sapient.com>
2019-05-22 14:17:39 +0530

    NumberSelector change to a class component

--- app/components/NumberSelector/NumberSelector.jsx
+++ app/components/NumberSelector/NumberSelector.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+class NumberSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedNumber: 0
+        }
+    }
+    componentWillMount() {
+        const selectedNumber = this.props.number;
+        this.setState({
+            selectedNumber
+        });
+    }
+    decrementNumber() {
+        let selectedNumber = this.state.selectedNumber;
+        selectedNumber -= 1;
+        this.setState({
+            selectedNumber: selectedNumber
+        });
+        console.log(this.state.selectedNumber)
+    }
+    incrementNumber() {
+        let selectedNumber = this.state.selectedNumber;
+        selectedNumber += 1;
+        this.setState({
+            selectedNumber: selectedNumber
+        });
+        console.log(this.state.selectedNumber)
+    }
+    render() {
+        const selectedNumberValue = this.state.selectedNumber;
 
-const NumberSelector = (props) => {
-    return (
-        <div className='number-selector'>
-            <button type="button" className="btn btn-primary btn-sm icon-plus" />
-            <span className="number-text">{props.number}</span>
-            <button type="button" className="btn btn-primary btn-sm icon-minus" />
-        </div >
-    )
-};
+        return (
+            <div className='number-selector'>
+                <button type="button" className="btn btn-primary btn-sm icon-minus" onClick={() => { this.decrementNumber() }} > </button>
+                <span className="number-text">{selectedNumberValue}</span>
+                <button type="button" className="btn btn-primary btn-sm icon-plus" onClick={() => { this.incrementNumber() }} > </button>
+            </div>
+        )
+    }
+}
 
 export default NumberSelector;
