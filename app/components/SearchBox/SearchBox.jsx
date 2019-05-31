6cbc526d03cec97ce9d6069b1452d98fa056c498
Arunesh Saxena <asaxena24@sapient.com>
2019-05-31 12:35:06 +0530

    onChangeHandler function implemented

--- app/components/SearchBox/SearchBox.jsx
+++ app/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const SearchBox = (props) => {
+    const onChangeHandler = (e) => {
+        props.changeHandler(e.target.value);
+    }
     return (
-        <div class="search-box">
+        <div className="search-box" >
             <label htmlFor="search-input">Search:</label>
-            <input type="text" id="search-input" placeholder="Search by name"></input>
+            <input
+                type="text"
+                id="search-input"
+                placeholder="Search by name"
+                onChange={(e) => onChangeHandler(e)} />
         </div>
     );
-}
+};
 
+SearchBox.propTypes = {};
 export default SearchBox;
