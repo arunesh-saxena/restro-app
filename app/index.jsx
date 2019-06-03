26a8cbe261430e0a4e2e1e846ec1278c29c99492
Arunesh Saxena <asaxena24@sapient.com>
2019-06-03 14:30:07 +0530

    update the preRenderMiddleware.js

--- app/index.jsx
+++ app/index.jsx
@@ -4,47 +4,29 @@ import { BrowserRouter, withRouter } from 'react-router-dom';
 import { renderRoutes, matchRoutes } from 'react-router-config';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import preRenderMiddleware from '../server/preRenderMiddleware';
 
 import createRoutes from './routes';
 const initialState = window.__INITIAL_STATE__;
 if (window.__INITIAL_STATE__ !== null) {
     window.__INITIAL_STATE__ = null;
 }
-const store = configureStore(initialState,{});
+const store = configureStore(initialState, {});
 const Routes = createRoutes(store);
-const loadData = (dispatch, branch, req, res) => {
-    // todo: need more refine use es6
-    const promises = [];
-    const promisesArray = branch.map(({ route, match }) => {
-        if (typeof route.need != 'undefined') {
-            // return dispatch(route.need[0](req.headers, res))
-            return route.need.map(need => {
-                promises.push(dispatch(need(req.headers, res)));
-            });
-        }
-    })
 
-    return Promise.all(promises)
-}
 const RouteDataLoader = withRouter(class extends React.Component {
- 
     componentWillReceiveProps(nextProps) {
         if (nextProps.location.pathname != this.props.location.pathname) {
             const branch = matchRoutes(
                 this.props.routes,
                 nextProps.location.pathname
             );
-            console.log(branch);
             const req = {
                 url: nextProps.location.pathname,
                 headers: {}
             };
-            const res = {
-                url: nextProps.location.pathname,
-                headers: {}
-            };
-            loadData(nextProps.dispatch, branch, req, res).then((v) => {
-                console.log('================Promise=========');
+            preRenderMiddleware(nextProps.dispatch, branch, req, null).then((v) => {
+                console.log('------------------Promise-end------------------');
             });
         }
     }
--- server/middleware.js
+++ server/middleware.js
@@ -2,7 +2,7 @@ import routes from '../app/routes.jsx';
 import { matchRoutes } from 'react-router-config';
 
 import pageRenderer from './pageRenderer';
-import { preRenderMiddleware } from './preRenderMiddleware';
+import preRenderMiddleware from './preRenderMiddleware';
 import configureStore from '../app/store/configureStore';
 
 /*
@@ -10,20 +10,6 @@ import configureStore from '../app/store/configureStore';
  * We grab the state passed in from the server and the req object from Express/Koa
  * and pass it into the Router.run function.
  */
-const loadData = (dispatch, branch, req, res) => {
-    // todo: need more refine use es6
-    const promises = [];
-    const promisesArray = branch.map(({ route, match }) => {
-        if (typeof route.need != 'undefined') {
-            // return dispatch(route.need[0](req.headers, res))
-            return route.need.map(need => {
-                promises.push(dispatch(need(req.headers, res)));
-            });
-        }
-    })
-
-    return Promise.all(promises)
-}
 export default function render(req, res) {
     /* todo: usefull for env config */
     // console.log(`process.env.NODE_ENV : ${process.env.NODE_ENV} | process.env.BUILD : ${process.env.BUILD}`);
@@ -31,30 +17,11 @@ export default function render(req, res) {
     const initialState = {};
     const store = configureStore(initialState);
 
-    /* --------START------- */
     const ROUTES = routes();
     const branch = matchRoutes(ROUTES, req.url);
-    loadData(store.dispatch, branch, req, res).then((v) => {
-        // console.log('================Promise=========', store.getState());
+    preRenderMiddleware(store.dispatch, branch, req, res).then((v) => {
         const html = pageRenderer(store, req, res);
         res.status(200).send(html);
     });
-    /* ---------END-------- */
-
-    /* const branch = matchRoutes(routes, req.url);
-    preRenderMiddleware(
-        store.dispatch,
-        branch,
-        req,
-        res,
-        'en'
-    )
-        .then(() => {
-            const html = pageRenderer(store, req.url);
-            res.status(200).send(html);
-        })
-        .catch(error => {
-            console.log(`error--- ${error}`);
-        }); */
 }
 
