eaac2c6ab714555582cf8d7bf629c2ae55bb251d
Arunesh Saxena <asaxena24@sapient.com>
2019-06-16 17:29:11 +0530

    add serverErrors components

---
+++ _front/app/components/serverErrors/ServerErrors.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ServerErrors extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div className="server-error">ServerErrors</div>;
+    }
+}
+
+// ServerErrors.PropTypes = {};
+
+export default ServerErrors;
