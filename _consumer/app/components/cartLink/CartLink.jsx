0ffd6ea63b1c8a1fcca9e82f580d4835a757ff4b
Arunesh Saxena <asaxena24@sapient.com>
2019-06-19 23:48:43 +0530

    cartLink item quantity with react context api

--- _consumer/app/components/cartLink/CartLink.jsx
+++ _consumer/app/components/cartLink/CartLink.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AppUrls from '../../appConstants/appUrls';
+import AppContext from '../context/AppContext';
 
 const CartLink = (props) => {
     const { headerLabel } = props;
     return (
-        <div>
-            <Link to={AppUrls.CART} title={headerLabel}>
-                {headerLabel}
-            </Link>
+        <div className="car-link">
+            <AppContext.Consumer>
+                {(context) => {
+                    const order = context.order;
+                    let cartItemNums = 0;
+                    order.forEach((item) => {
+                        cartItemNums += item.quantity;
+                    });
+                    return (
+                        <Link to={AppUrls.CART} title={headerLabel}>
+                            {headerLabel} {cartItemNums ? cartItemNums : ''}
+                        </Link>
+                    );
+                }}
+            </AppContext.Consumer>
         </div>
     );
 };
--- _consumer/app/pages/App.jsx
+++ _consumer/app/pages/App.jsx
@@ -3,12 +3,11 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AppContainer from '../containers/AppContainer';
 import appConstants from '../appConstants/appConstants';
+import AppContext from '../components/context/AppContext';
 
 class App extends React.Component {
-    constructor() {
-        super();
-    }
     render() {
+        const cart = this.props.cart;
         const props = Object.assign({}, this.props, {
             labels: appConstants.labels,
             handleLogout: this.handleLogout
@@ -16,13 +15,17 @@ class App extends React.Component {
 
         return (
             <div className="app-page">
-                <AppContainer {...props} />
+                <AppContext.Provider value={cart}>
+                    <AppContainer {...props} />
+                </AppContext.Provider>
             </div>
         );
     }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+    cart: state.cart
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
