e260cc3adb5d0f917107a2d268a3b00368f04478
Arunesh Saxena <asaxena24@sapient.com>
2019-06-23 19:00:21 +0530

    orderDetailsTable component implemented

---
+++ _consumer/app/components/orderDetailTable/OrderDetailTable.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OrderDetailTable = (props) => {
+    const {
+        id,
+        tokenId,
+        totalCost,
+        status: orderStatus,
+        tableId,
+        items,
+        updatedAt
+    } = props.orderDetail || {};
+    return (
+        <div className="row justify-content-md-center">
+            <div className="col-6">
+                <div className="order-detail-table">
+                    <div className="table-row">
+                        <div>Order Id</div>
+                        <div>{id}</div>
+                    </div>
+                    <div className="table-row">
+                        <div>Token Id</div>
+                        <div>{tokenId}</div>
+                    </div>
+                    <div className="table-row">
+                        <div>Table Id</div>
+                        <div>{tableId}</div>
+                    </div>
+                    <div className="table-row">
+                        <div>Order Status</div>
+                        <div>{orderStatus}</div>
+                    </div>
+                    <div className="table-row">
+                        <div>Total Cost</div>
+                        <div>{totalCost}</div>
+                    </div>
+                    <div className="table-row">
+                        <div>Last updatedAt</div>
+                        <div>{updatedAt}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+OrderDetailTable.prototype = {
+    orderDetail: PropTypes.object
+};
+
+export default OrderDetailTable;
