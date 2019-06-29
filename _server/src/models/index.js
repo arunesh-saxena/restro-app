f15ff9e94f3a2ca4dcae9498c24852e82f3b200b
Arunesh Saxena <asaxena24@sapient.com>
2019-06-29 18:46:54 +0530

    restaurant scheme add

--- _server/src/models/index.js
+++ _server/src/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./User');
 const Menu = require('./restro/Menu');
 const Order = require('./restro/Order');
+const Restaurant = require('./restro/Restaurant');
 
 module.exports = {
     User,
     Menu,
-    Order
+    Order,
+    Restaurant
 };
---
+++ _server/src/models/restro/Restaurant.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+    id: { type: Number, default: 1 },
+    restaurantCode: { type: String, default: null },
+    restaurantName: { type: String, require: true },
+    noOfTables: { type: Number, required: true },
+    isHidden: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+    createdAt: { type: Date },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+restaurantSchema.pre('save', function (next) {
+    const doc = this;
+    Restaurant.find({})
+        .select('id')
+        .sort({ id: -1 })
+        .limit(1)
+        .exec((err, data) => {
+            if (data.length) {
+                doc.id = ++data[0].id;
+                doc.restaurantCode = `${doc.restaurantName.substr(0, 3)}${
+                    doc.id
+                }`;
+            } else {
+                doc.restaurantCode = `${doc.restaurantName.substr(0, 3)}${
+                    doc.id
+                }`;
+            }
+            doc.createdAt = Date.now();
+            next();
+        });
+});
+
+restaurantSchema.pre('update', function (next) {
+    console.log('todo: this is not working on findOneandUpdate');
+    this.update(
+        {},
+        {
+            $set: { updatedAt: new Date() }
+        }
+    );
+});
+
+const Restaurant = mongoose.model('restaurant', restaurantSchema);
+
+module.exports = Restaurant;
